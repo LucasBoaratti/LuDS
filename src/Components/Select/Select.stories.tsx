a2302c34879afa5964d83fd30871db7cc9dddbd4
Lucas Boaratti <lucasboaratti15@gmail.com>
2026-06-17 08:51:55 -0300

    Criando o story para o select.

---
+++ src/Components/Select/Select.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Select } from "./Select";
+
+const meta: Meta<typeof Select> = {
+    title: "Componentes/Select",
+    component: Select,
+}
+
+export default meta;
+
+type Story = StoryObj<typeof Select>;
+
+// Estados do select
+export const Default: Story = {
+    args: {
+        label: "Sexo:",
+        id: "sexo",
+        options: [
+            {
+                label: "Masculino",
+                value: "masculino",
+            },
+            {
+                label: "Feminino",
+                value: "feminino",
+            },
+            {
+                label: "Outros",
+                value: "outros",
+            },
+        ],
+    }
+}
